56ddee6bcafd3a973280111fddec3ee9d0504dd4
Thomas Nind <31306100+tznind@users.noreply.github.com>
2020-11-11 22:06:48 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using BadMedicine;
+using BadMedicine.Datasets;
+using System.Text;
+using System.IO;
+
+namespace BadMedicineExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var rand = new Random(500/*seed*/);
+
+            var pc = new PersonCollection();
+            
+            // Generate 100 people
+            pc.GeneratePeople(100,rand);
+
+            // Generate a Biochemistry record
+            var biochemRecord = new BiochemistryRecord(rand);
+
+            //Get a random person from the collection
+            var person = pc.People[rand.Next(pc.People.Length)];
+
+            // Output some custom Xml format
+            using(var writer = new StreamWriter("out.xml"))
+            {
+                writer.WriteLine("<Record>");
+                
+                writer.WriteLine($"<Date>{person.GetRandomDateDuringLifetime(rand)}</Date>");
+                writer.WriteLine($"<SampleType>{biochemRecord.SampleType}</SampleType>");
+                writer.WriteLine($"<TestCode>{biochemRecord.TestCode}</TestCode>");
+                writer.WriteLine($"<Result>{biochemRecord.Result}</Result>");
+                writer.WriteLine($"<Identifier>{biochemRecord.LabNumber}</Identifier>");
+                writer.WriteLine($"<ReadCode>{biochemRecord.ReadCodeValue}</ReadCode>");
+                
+                writer.WriteLine("</Record>");
+
+            }
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,37 @@
+# Summary
+ This repository demonstrates how to use [BadMedicine](https://github.com/HicServices/BadMedicine) nuget package to generate custom EHR file formats (in this example we use XML).  In the example a single Biochemistry record is generated and written out to a file called `out.xml`.
+ 
+# How it was done
+ 
+ To follow this demo you will need [c# dotnet core 3.1](https://dotnet.microsoft.com/download/dotnet-core/3.1)
+ 
+ This repository was created using the following commands:
+ 
+ ```
+ dotnet new console
+ dotnet add package HIC.BadMedicine
+ ```
+ 
+Build with:
+ 
+```
+dotnet build
+```
+
+Run with:
+```
+dotnet run
+```
+
+When run a new file out.xml should be generated e.g.
+ 
+ ```xml
+ <Record>
+<Date>27/02/1994 00:17:16</Date>
+<SampleType>Blood</SampleType>
+<TestCode>eCOM</TestCode>
+<Result>Blood</Result>
+<Identifier>CC319477</Identifier>
+<ReadCode>451E.</ReadCode>
+</Record>
+```
